5b6487b59b4b2da01a94a1fe147c7c53bf3e1b38
SoulArt Limited <soulart2024@gmail.com>
2026-03-12 16:26:04 +0000

    Initial Commit

---
+++ SoulArtTempleNativeClean/SoulArtTempleNativeCleanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoulArtTempleNativeCleanApp.swift
+//  SoulArtTempleNativeClean
+//
+//  Created by Soraya Roberts on 12/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct SoulArtTempleNativeCleanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
